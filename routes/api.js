b5fa64225b913f02bfd1edd7d4017a71aab50e84
niuhongtao <18333602833@163.com>
2022-12-02 18:44:03 +0800

    增加/address

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,19 @@
 const express = require("express");
 
-var router = express.Router()
+var router = express.Router();
 
-router.get("/",(req,res)=>{
-   res.send("api接口")
- })
- 
- module.exports = router
+router.get("/", (req, res) => {
+  res.send("api接口");
+});
+
+router.get("/address", function (req, res, next) {
+  res.send({
+    success: true,
+    result: [
+      { name: "张三", address: "北京市" },
+      { name: "李四", address: "北京市" },
+      { name: "王五", address: "北京市" },
+    ],
+  });
+});
+module.exports = router;
